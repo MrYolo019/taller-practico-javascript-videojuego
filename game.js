efe490e43a26252aba357b3358d3b3339265a3ff
MrYolo019 <Silvatj08@gmail.com>
2022-10-26 09:03:25 -0500

    Se ubica al PLAYER y se le asignan los mivimientos sin la posibilidad de salir del canvas

--- game.js
+++ game.js
@@ -1,11 +1,21 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down'); 
+
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
 
 let canvasSize; 
 let elementsSize;
+
+let playerPosition = {
+    x: undefined,
+    y: undefined,
+}
 
 function setCanvasSize() {
     if(window.innerHeight > innerWidth){
@@ -22,17 +32,87 @@ function setCanvasSize() {
     startGame();
 }
 
-const map = maps[0];
-const mapRows = map.trim().split('\n');
-const mapRowCols = mapRows.map(row => row.trim().split(''));
-
 function startGame() {
     game.font = (elementsSize - 12) + 'px Verdanada';
     game.textAlign = 'end';
 
-     for (let row = 1; row <= 10; row++) {
-        for (let col = 0; col <=10; col++) {
-        game.fillText(emojis[mapRowCols[row -1][col -1]], elementsSize * col, elementsSize * row);
-        }
+    const map = maps[0];
+    const mapRows = map.trim().split('\n');
+    const mapRowCols = mapRows.map(row => row.trim().split(''));
+
+    game.clearRect(0,0, canvasSize, canvasSize);
+    mapRowCols.forEach((row, rowI) => {
+        row.forEach((col, colI) => {
+            const emoji = emojis[col];
+            const posX = elementsSize * (colI + 1);
+            const posY = elementsSize * (rowI + 1);
+
+            if (col == 'O') {
+                if (!playerPosition.x && !playerPosition.y){
+                    playerPosition.x = posX;
+                    playerPosition.y = posY;
+                    console.log(playerPosition);
+                }
+            }
+
+            game.fillText(emoji, posX, posY);
+        });
+    });
+
+    movePlayer();
+}
+
+function movePlayer(){
+    game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y)
+}
+
+window.addEventListener('keydown', moveByKeys);
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+function moveByKeys(event){
+    if(event.key == 'ArrowUp') moveUp();
+    else if (event.key == 'ArrowLeft') moveLeft();
+    else if (event.key == 'ArrowRight') moveRight();
+    else if (event.key == 'ArrowDown') moveDown();
+}
+
+function moveUp(){
+    if ((playerPosition.y - elementsSize) < elementsSize){
+        console.log('OUT');
+    } else {
+        playerPosition.y -= elementsSize;
+        startGame();   
     }
 }
+
+function moveLeft(){
+    if ((playerPosition.x - elementsSize) < elementsSize){
+        console.log('NOT');
+    } else{
+        playerPosition.x -= elementsSize;
+        startGame();
+    }
+}
+
+function moveRight(){
+    if ((playerPosition.x + elementsSize) > canvasSize){
+        console.log('NOT');
+    } else{
+        playerPosition.x += elementsSize;
+        startGame();
+    }
+}
+
+function moveDown(){
+    if ((playerPosition.y + elementsSize) > canvasSize){
+        console.log('OUT');
+    } else {
+        playerPosition.y += elementsSize;
+        startGame();   
+    }
+}
+
+
