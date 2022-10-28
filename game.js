7e90d9fbd95d9de9e66610f195f3402aac6d884f
MrYolo019 <Silvatj08@gmail.com>
2022-10-28 10:27:34 -0500

    Se acomoda el juego para que al chocar con bobombas se reinicie y se agrega sistema de vidas

--- game.js
+++ game.js
@@ -4,6 +4,9 @@ const btnUp = document.querySelector('#up');
 const btnLeft = document.querySelector('#left');
 const btnRight = document.querySelector('#right');
 const btnDown = document.querySelector('#down'); 
+const spanLives = document.querySelector('#lives');
+const spanTime = document.querySelector('#time');
+
 
 
 window.addEventListener('load', setCanvasSize);
@@ -11,6 +14,12 @@ window.addEventListener('resize', setCanvasSize);
 
 let canvasSize; 
 let elementsSize;
+let level = 0;
+let lives = 3;
+
+let timeStart;
+let timePlayer;
+let timeInterval;
 
 const playerPosition = {
     x: undefined,
@@ -20,7 +29,7 @@ const giftPosition = {
     x: undefined,
     y: undefined,
 }
-const enemiesPositions = [];
+let enemiesPositions = [];
 
 function setCanvasSize() {
     if(window.innerHeight > innerWidth){
@@ -41,10 +50,24 @@ function startGame() {
     game.font = (elementsSize - 12) + 'px Verdanada';
     game.textAlign = 'end';
 
-    const map = maps[0];
+    const map = maps[level];
+
+    if (!map) {
+        gameWin();
+        return;
+    }
+
+    if (!timeStart) {
+        timeStart = Date.now();
+        timeInterval = setInterval( showTime,100);
+    }
+
     const mapRows = map.trim().split('\n');
     const mapRowCols = mapRows.map(row => row.trim().split(''));
 
+    showLives();
+
+    enemiesPositions = [];
     game.clearRect(0,0, canvasSize, canvasSize);
     mapRowCols.forEach((row, rowI) => {
         row.forEach((col, colI) => {
@@ -67,7 +90,6 @@ function startGame() {
                     y: posY,
                 });
             }
-
             game.fillText(emoji, posX, posY);
         });
     });
@@ -81,10 +103,57 @@ function movePlayer(){
     const giftCollision = giftCollisionX && giftCollisionY;
 
     if (giftCollision){
-        console.log('Ganaste');
+        levelWin()
+    }
+
+    const enemyCollision = enemiesPositions.find(enemy => {
+        const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
+        const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
+        return enemyCollisionX && enemyCollisionY;
+    });
+    
+    if (enemyCollision){
+        levelFail();
     }
     
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y)
+}
+
+function levelWin(){
+    console.log('subiste de nivel');
+    level++;
+    startGame();
+}
+
+function levelFail(){
+    lives--;
+    if (lives <= 0) {
+        level = 0;
+        lives = 3;
+        timeStart = undefined;
+    }
+
+    playerPosition.x = undefined;
+    playerPosition.y = undefined;
+    startGame();
+
+}
+
+function gameWin(){
+    console.log('TERMINASTE');
+    clearInterval(timeInterval);
+}
+
+function showLives() {
+    const heartsArray = Array(lives).fill(emojis['HEART']); 
+    
+    spanLives.innerHTML = '';
+    heartsArray.forEach(heart => spanLives.append(heart));
+    // spanLives.innerHTML = ; 
+}
+
+function showTime () {
+    spanTime.innerHTML = Date.now() - timeStart;
 }
 
 window.addEventListener('keydown', moveByKeys);
